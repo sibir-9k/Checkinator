06998d800cee7c1af07e9d34588ddf86115f5fc8
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2021-11-07 14:40:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const [{ Server: h1 }, x] = [require('http'), require('express')];
+const { privateDecrypt: dec } = require('crypto');
+const Busboy = require('busboy');
+
+const Router = x.Router();
+const PORT = 10001;
+
+const { log } = console;
+const app = x();
+Router
+  .route('/')
+  .get(r => r.res.end('Привет мир!'))
+  .post(async (req, res) => {
+    console.log(req.headers);
+    let o = {key: '', secret: []};
+    const boy = new Busboy({ headers: req.headers });
+    boy.on('file', (fieldname, file) => file
+      .on('data', data => {
+             if (fieldname == 'key') {
+                 o[fieldname] += data;
+             } else {
+                 o[fieldname].push(data);
+             }
+       }));
+    boy.on('finish', () => {
+      o.secret = Buffer.concat(o.secret);
+      let result;
+      try {
+           result = dec(o.key, o.secret);
+      } catch(e) {
+           result = 'ERROR!';
+      }
+      res
+      /* .set(CORS) */
+      .send(String(result));
+    });
+    req.pipe(boy);
+
+  });
+app
+  .use('/', Router)
+  .use(({ res: r }) => r.status(404).send('Пока нет!'))
+  .use((e, r, rs, n) => rs.status(500).send(`Ошибка: ${e}`))
+  .set('x-powered-by', false);
+module.exports = h1(app)
+  .listen(process.env.PORT || PORT, () => log(process.pid));
